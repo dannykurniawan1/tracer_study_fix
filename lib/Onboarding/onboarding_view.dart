6e9a70e1e3930960c5f67eaa8ef5707b06c13daf
danny <dannykurniawan1>
2024-04-12 20:55:18 +0700

    progres

--- lib/Onboarding/onboarding_view.dart
+++ lib/Onboarding/onboarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
@@ -9,15 +10,84 @@ class OnboardingView extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Image.asset(
-              'assets/images/onboarding1.png',
-              height: 250,
+            Expanded(
+              child: PageView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) => OnboardContent(
+                  image: 'assets/images/onboarding1.png',
+                  title: 'Pemberitahuan',
+                  description:
+                      'Alumni yang bisa mengakses adalah alumni yang telah di daftarkan oleh pihak kampus',
+                ),
+              ),
             ),
-            Text('Pemberitahuan',style: Theme.of(context)
-            .textTheme.headline5!.copyWith(fontWeight: FontWeight.w500),)
+            SizedBox(
+              height: 60,
+              width: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: SvgPicture.asset(
+                  'assets/svg/Vector1.svg',
+                  height: 30,
+                  width: 30,
+                ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class OnboardContent extends StatelessWidget {
+  const OnboardContent({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  final String image;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Spacer(),
+        Image.asset(
+          image,
+          height: 250,
+        ),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              color: Colors.black),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          description,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Colors.black),
+        ),
+        const Spacer(),
+      ],
     );
   }
 }
